70aec257cfe7b6bc0d67275bd14d4eb38301feec
unknown <jamal99mohamed@gmail.com>
2024-05-21 20:06:59 +0100

    hii

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/menu_page.dart';
 import 'pages/page_intro.dart';
 
 void main() {
@@ -11,10 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pizzeria App',
       home: Page_intro(),
+      routes: {
+        '/intropage': (context) => const Page_intro(),
+        '/menupage': (context) => const Menu_page(),
+      },
     );
   }
 }
--- lib/pages/page_intro.dart
+++ lib/pages/page_intro.dart
@@ -1,4 +1,7 @@
+import 'package:app1pizza/compenents/button.dart';
+import 'package:app1pizza/pages/menu_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Page_intro extends StatelessWidget {
@@ -7,40 +10,67 @@ class Page_intro extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 213, 200),
       body: Padding(
         padding: EdgeInsets.all(25.0),
-        child: Column(children: [
-          SizedBox(
-            height: 25,
-          ),
-          // pizzeria name
-          // Text(
-          //   "Pizzeria J&Y&D",
-          //   style: GoogleFonts.merienda(fontSize: 28),
-          // ),
-          Text('Pizzeria J&Y&D',
-              style: TextStyle(
-                  fontFamily: 'dawingOfANewDay',
-                  fontSize: 30,
-                  color: Color.fromARGB(255, 45, 179, 114))),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              // pizzeria name
+              Text(
+                "Pizzeria J&Y&D",
+                style: GoogleFonts.merienda(
+                    fontSize: 44, color: Color.fromARGB(255, 45, 179, 114)),
+              ),
 
-          SizedBox(
-            height: 25,
-          ),
-          // image
-          Image.asset('lib/images/pizza1&&'),
+              const SizedBox(
+                height: 30,
+              ),
+              // image
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Image.asset('lib/images/pizza1.png'),
+              ),
 
-          SizedBox(
-            height: 25,
-          ),
-          // ttitle
+              SizedBox(
+                height: 25,
+              ),
+              // ttitle
+              Text(
+                "THE TASTE OF ITALIAN PIZZA",
+                style: GoogleFonts.dmSerifDisplay(
+                    fontSize: 44,
+                    color: Color.fromARGB(255, 128, 96, 96),
+                    height: 1.1),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              // txt
+              Text(
+                "feel the taste of the most delicious italian pizza from anywhere and any time",
+                style: GoogleFonts.dmSerifDisplay(
+                  height: 1.5,
+                  color: Colors.grey[600],
+                ),
+              ),
 
-          // txt
-
-          // button start
-        ]),
+              SizedBox(
+                height: 50,
+              ),
+              // button start
+              Mybutton(
+                txt: "Get Started ",
+                onTap: () {
+                  Navigator.pushNamed(context, '/menupage');
+                },
+              ),
+            ]),
       ),
     );
   }
